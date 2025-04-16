66b19c09f4d59b103a9f99f2e3f6423ce04eaee2
khafidhasukma <khafidhasukma@gmail.com>
2025-04-16 20:04:33 +0700

    feat: update Palindrome class to enhance user input handling and display results

--- week5/Palindrome.java
+++ week5/Palindrome.java
@@ -4,16 +4,19 @@ import java.util.Scanner;
 
 public class Palindrome {
     public static void main(String[] args) {
+        // input dari pengguna
         Scanner scanner = new Scanner(System.in);
-        
         System.out.print("Masukkan kata: ");
         String input = scanner.nextLine();
-
-        TextGame game = new TextGame();
-        boolean isPalindrome = game.isPalindrome(input);
-
-        System.out.println("Hasil: " + (isPalindrome ? "Palindrome" : "Bukan Palindrome"));
         
+        TextGame game1 = new TextGame();
+        game1.displayResult(input);
+
+        System.out.println();
+
+        TextGame game2 = new FancyTextGame();
+        game2.displayResult(input);
+
         scanner.close();
     }
 }
